83334d2c93b98bdc52b5bea4aed563ec0708b865
aslibeyhan <asli.beyhann@gmail.com>
2024-01-13 14:05:41 +0300

    react-redux and contex api intro

---
+++ src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,36 @@
+
+import {useEffect, useState} from "react";
+import {ProductModel} from "../../models/responses/ProductModel";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import ProductService from "../../services/ProductService";
+
+type Props = {};
+
+const Homepage = (props: Props) => {
+	const [products, setProducts] = useState<ProductModel[]>([]);
+
+	useEffect(() => {
+		fetchProducts();
+	}, []);
+
+	const fetchProducts = () => {
+		let service: ProductService = new ProductService();
+		service.getAll().then(response => {
+			setProducts(response.data.products);
+		});
+	};
+
+	return (
+		<div className="container">
+			<div className="row">
+				{products.map(product => (
+					<div key={product.id} className="col-3">
+						<ProductCard product={product} />
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default Homepage;
